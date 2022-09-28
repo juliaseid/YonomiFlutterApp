24c347767908a50220cf558f42c10328ab71afdf
Julia Seidman <juliaseid@gmail.com>
2022-09-28 12:21:42 -0700

    Lock factory functions for traits

--- lib/models/devicelist.dart
+++ lib/models/devicelist.dart
@@ -1,72 +1,81 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:yonomi_device_widgets/providers/device_provider.dart';
 import 'package:yonomi_flutter_app/models/request.dart';
-import 'package:yonomi_flutter_app/models/device.dart';
 import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
 
 class DeviceList {
-  // TODO: set up constructor
-  DeviceList._create(String jsonData) {
-    //TODO: parse json
-  }
+  List<Device>? myDevices;
+
+  DeviceList._create(List<Device> deviceList);
 
   static Future<DeviceList> create() async {
-    String jsonData = DevicesRepository.getDevices(request).toString();
-    var deviceList = DeviceList._create(jsonData);
-    return deviceList;
-  }
-}
-
-class Device {
-  String? deviceId;
-  String? deviceName;
-  String? deviceType;
-
-  Device({this.deviceId, this.deviceName, this.deviceType});
-
-  factory Device.fromRawJson(String str) => Device.fromJson(json.decode(str));
-
-  factory Device.fromJson(Map<String, dynamic> json) {
-    String type = "other";
-    String trait = json['traits'][0];
-    if (trait == 'THERMOSTAT_SETTING') {
-      type = "thermostat";
-    } else if (trait == 'LOCK') {
-      type = "lock";
-    } else if (trait == "BRIGHTNESS") {
-      type = "light";
-    }
-    return Device(
-      deviceId: json['id'],
-      deviceName: json['displayName'],
-      deviceType: type,
-    );
+    List<Device> deviceList = await DevicesRepository.getDevices(request);
+    var myDevices = DeviceList._create(deviceList);
+    return myDevices;
   }
 }
 
 class Lock with ChangeNotifier {
   String? lockId;
   String? lockName;
-  String? lockedState;
-  String? jammedState;
+  bool? lockedState;
+  bool? jammedState;
   int? batteryLevel;
 
-  Lock._create(String jsonData) {
-    //TODO: parse JSON data & assign
-    this.lockId;
-    this.lockName;
-    this.lockedState;
-    this.jammedState;
-    this.batteryLevel;
+  Lock._create(Device lock) {
+    lockId = lock.id;
+    lockName = lock.displayName;
+
+    Trait isLocked =
+        (lock.traits.singleWhere((element) => (element.name == "isLocked")));
+    isLocked.properties.first.value ? lockedState = true : lockedState = false;
+
+    Trait supportsIsJammed = (lock.traits
+        .singleWhere((element) => (element.name) == "supportsIsJammed"));
+    Trait? isJammed =
+        (lock.traits.singleWhere((element) => (element.name) == "isJammed"));
+    supportsIsJammed.properties.first.value
+        ? jammedState = isJammed.properties.first.value
+        : jammedState = null;
+
+    batteryLevel = lock.traits
+        .singleWhere(((element) => (element.name == "batteryLevel")))
+        .properties
+        .first
+        .value;
   }
 
   static Future<Lock> create(String lockId) async {
-    String jsonData =
-        DevicesRepository.getLockDetails(request, lockId).toString();
-    var lockDetails = Lock._create(jsonData);
-    return lockDetails;
+    Device lockDevice = await DevicesRepository.getLockDetails(request, lockId);
+    var lock = Lock._create(lockDevice);
+    return lock;
   }
 }
+
+// class Device {
+//   String? deviceId;
+//   String? deviceName;
+//   String? deviceType;
+
+//   Device({this.deviceId, this.deviceName, this.deviceType});
+
+//   factory Device.fromRawJson(String str) => Device.fromJson(json.decode(str));
+
+//   factory Device.fromJson(Map<String, dynamic> json) {
+//     String type = "other";
+//     String trait = json['traits'][0];
+//     if (trait == 'THERMOSTAT_SETTING') {
+//       type = "thermostat";
+//     } else if (trait == 'LOCK') {
+//       type = "lock";
+//     } else if (trait == "BRIGHTNESS") {
+//       type = "light";
+//     }
+//     return Device(
+//       deviceId: json['id'],
+//       deviceName: json['displayName'],
+//       deviceType: type,
+//     );
+//   }
+// }
