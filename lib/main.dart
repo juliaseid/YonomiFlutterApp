65ddb40eda859b23233b4a5df54471af63ec15da
Julia Seidman <juliaseid@gmail.com>
2022-09-30 15:26:52 -0700

    start trying to figure out Flutter pieces

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:window_size/window_size.dart';
+import 'package:yonomi_flutter_app/common/theme.dart';
 //Set window size for builds identified by dart:io and kIsWeb criteria
 import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
 import 'package:yonomi_flutter_app/models/devicelist.dart';
+import 'package:yonomi_flutter_app/models/device.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async {
@@ -41,59 +43,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Yonomi Device App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
+    return Provider<DeviceList>(
+        create: (context) => DeviceList.create(),
+        child: MaterialApp(
+          title: 'Yonomi Device Demo App', 
+          theme: appTheme,
+          initialRoute: '/',
+          routes: {
+            '/': ((context) => const MyDeviceList(),
+            '/deviceDetail': (context) => const MyDeviceDetail(),
+          }
+        )
+      );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureProvider<MyModel>(
-      //                      <--- FutureProvider
-      initialData: MyModel(someValue: 'default value'),
-      create: (context) => someAsyncFunctionToGetMyModel(),
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: Text('My App')),
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                  padding: const EdgeInsets.all(20),
-                  color: Colors.green[200],
-                  child: Consumer<MyModel>(
-                    //                    <--- Consumer
-                    builder: (context, myModel, child) {
-                      return RaisedButton(
-                        child: Text('Do something'),
-                        onPressed: () {
-                          myModel.doSomething();
-                        },
-                      );
-                    },
-                  )),
-              Container(
-                padding: const EdgeInsets.all(35),
-                color: Colors.blue[200],
-                child: Consumer<MyModel>(
-                  //                    <--- Consumer
-                  builder: (context, myModel, child) {
-                    return Text(myModel.someValue);
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
--- lib/models/devicelist.dart
+++ lib/models/devicelist.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
 import 'package:yonomi_flutter_app/models/request.dart';
 import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
 
@@ -9,55 +9,18 @@ class DeviceList {
 
   DeviceList._create(List<Device> deviceList);
 
-  static Future<DeviceList> create() async {
-    List<Device> deviceList = await DevicesRepository.getDevices(request);
+  static DeviceList create() {
+    var deviceList = <Device>[];
+    DevicesRepository.getDevices(request)
+        .then(((value) => {deviceList = value}))
+        .catchError((e) {
+      print('Got error: $e');
+    });
     var myDevices = DeviceList._create(deviceList);
     return myDevices;
   }
 }
 
-class Lock with ChangeNotifier {
-  String? lockId;
-  String? lockName;
-  bool? lockedState;
-  bool? jammedState;
-  int? batteryLevel;
-
-  Lock._create(Device lock) {
-    lockId = lock.id;
-    lockName = lock.displayName;
-
-    Trait isLocked =
-        (lock.traits.singleWhere((element) => (element.name == "isLocked")));
-    isLocked.properties.first.value ? lockedState = true : lockedState = false;
-
-    Trait supportsIsJammed = (lock.traits
-        .singleWhere((element) => (element.name) == "supportsIsJammed"));
-    Trait? isJammed =
-        (lock.traits.singleWhere((element) => (element.name) == "isJammed"));
-    supportsIsJammed.properties.first.value
-        ? jammedState = isJammed.properties.first.value
-        : jammedState = null;
-
-    batteryLevel = lock.traits
-        .singleWhere(((element) => (element.name == "batteryLevel")))
-        .properties
-        .first
-        .value;
-  }
-
-  // static Future<Lock> create(String lockId) async {
-  //   Device lockDevice = await DevicesRepository.getLockDetails(request, lockId);
-  //   var lock = Lock._create(lockDevice);
-  //   return lock;
-  // }
-
-  static Lock create(String lockId) async {
-    var newLock = Lock()
-    //TODO: create an empty lock for initial state, then call Yonomi
-  }
-
-}
 
 // class Device {
 //   String? deviceId;
