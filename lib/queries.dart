c58feadbe6b7b0b3aeed3938c0f9c01f2f34f81a
Julia Seidman <juliaseid@gmail.com>
2022-11-22 16:45:58 -0800

    Model GetDeviceList on DeviceProvider from widgets SDK

--- lib/queries.dart
+++ lib/queries.dart
@@ -1,21 +1,69 @@
 import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
-import 'package:new_app/request.dart';
+import 'package:yonomi_flutter_app/request.dart';
+import 'package:yonomi_device_widgets/providers/widget_state.dart';
+import 'package:yonomi_device_widgets/ui/string_constants.dart';
+
+typedef GetDeviceListMethod = Future<List<Device>> Function(Request request);
 
 class DeviceList {
-  List<Device> myDevices = [];
+  late Request _request;
 
-  DeviceList._create(List<Device> deviceList);
+  List<Device>? _deviceList;
+  WidgetState _state = WidgetState.idle;
+  String _errorMsg = StringConstants.AN_ERROR_OCCURRED;
+  bool _isDisposed = false;
 
-  static DeviceList create() {
-    var deviceList = <Device>[];
-    DevicesRepository.getDevices(request)
-        .then(((value) => {deviceList = value}))
-        .catchError((e) {
-      print('Got error: $e');
-    });
-    var myDevices = DeviceList._create(deviceList);
-    return myDevices;
+  DeviceList(Request request,
+      {GetDeviceListMethod getDeviceList = DevicesRepository.getDevices}) {
+    this._request = request;
+    fetchData(getDeviceList: getDeviceList);
   }
+
+  Future<void> fetchData(
+      {GetDeviceListMethod getDeviceList =
+          DevicesRepository.getDevices}) async {
+    setState = WidgetState.loading;
+
+    try {
+      _deviceList = await getDeviceList(_request);
+      setState = WidgetState.idle;
+    } catch (error) {
+      setErrorState(error.toString());
+    }
+  }
+
+  List<Device>? get myDevices => _deviceList;
+
+  set setState(WidgetState newState) {
+    if (!this._isDisposed) {
+      _state = newState;
+    }
+  }
+
+  void dispose() {
+    this._isDisposed = true;
+  }
+
+  void setErrorState(String errorMsg) {
+    _setErrorMessage = errorMsg;
+    setState = WidgetState.error;
+  }
+
+  set _setErrorMessage(String errorMsg) {
+    if (errorMsg.isEmpty) errorMsg = "An error occurred.";
+    _errorMsg = errorMsg;
+  }
+
+  bool get isInErrorState => _state == WidgetState.error;
+
+  bool get isLoading => _state == WidgetState.loading;
+
+  bool get isPerformingAction => _state == WidgetState.performingAction;
+
+  bool get isBusy =>
+      (_state == WidgetState.loading || _state == WidgetState.performingAction);
+
+  String get getErrorMessage => _errorMsg;
 }
 
 //How to handle reqeusts that aren't built into the SDK methods? I guess just a regular API call specifying endpoint and headers as properties of Request class
