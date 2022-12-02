950c199e6b502c1b35cbbfdf926941c13cf0d221
Julia Seidman <juliaseid@gmail.com>
2022-12-02 14:52:52 -0800

    creates 2 separate paths to resolve Future, removes DeviceList class from main.dart

--- lib/devicelist.dart
+++ lib/devicelist.dart
@@ -7,37 +7,23 @@ Iterable<String> path = ['.env'];
 var env = DotEnv(includePlatformEnvironment: true)..load(path);
 
 class DeviceList {
-  String _myDevices = "";
+  List<Device> _myDevices = [];
   final Request request;
 
   DeviceList(this.request);
 
   Future<void> fetchDeviceList() async {
-    final devicesFromGraph =
-        await DevicesRepository.getDevices(request).toString();
+    final devicesFromGraph = await DevicesRepository.getDevices(request);
     if (devicesFromGraph == null) {
-      _myDevices = "";
+      _myDevices = [];
       return;
     }
     _myDevices = devicesFromGraph;
-    // .map((device) => Device(
-    //     device.id,
-    //     device.displayName,
-    //     device.description,
-    //     device.manufacturerName,
-    //     device.model,
-    //     device.serialNumber,
-    //     device.createdAt,
-    //     device.updatedAt,
-    //     device.traits))
-    // .toList();
   }
 
-  String get myDevices => _myDevices;
+  List<Device> get myDevices => _myDevices;
 }
 
 main() async {
-  // print(DeviceList(request).fetchDeviceList());
-  print(DevicesRepository.getDeviceDetails(
-      request, "13334f06-3b7d-423a-a7a3-f81257544920"));
+  DeviceList(request).fetchDeviceList();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:yonomi_device_widgets/traits/detail_screen.dart';
 import 'package:yonomi_flutter_app/request.dart';
-// import 'package:yonomi_flutter_app/devicelist.dart';
-import 'package:yonomi_platform_sdk/yonomi-sdk.dart' as SDK;
+import 'package:yonomi_flutter_app/devicelist.dart';
+import 'package:yonomi_platform_sdk/yonomi-sdk.dart' as platform_SDK;
 
 void main() => runApp(const MyApp());
 
@@ -14,13 +14,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<List<SDK.Device>> futureDeviceList;
+  late Future<List<platform_SDK.Device>> futureDeviceList;
   late Future<String> testString;
 
   @override
   void initState() {
     super.initState();
-    futureDeviceList = SDK.DevicesRepository.getDevices(request);
+    futureDeviceList = platform_SDK.DevicesRepository.getDevices(request);
     testString = fetchHealthCheck();
   }
 
@@ -36,13 +36,13 @@ class _MyAppState extends State<MyApp> {
               title: const Text('Welcome to Yonomi'),
             ),
             body: Center(
-                child: FutureBuilder<List<SDK.Device>>(
+                child: FutureBuilder<List<platform_SDK.Device>>(
                     future: futureDeviceList,
-                    builder:
-                        ((context, AsyncSnapshot<List<SDK.Device>> snapshot) {
+                    builder: ((context,
+                        AsyncSnapshot<List<platform_SDK.Device>> snapshot) {
                       List<Widget> children;
                       if (snapshot.hasData) {
-                        List<SDK.Device> devices = snapshot.data!;
+                        List<platform_SDK.Device> devices = snapshot.data!;
                         children = <Widget>[
                           const Text("Your Yonomi Devices"),
                           Padding(
@@ -103,50 +103,6 @@ class _MyAppState extends State<MyApp> {
                           children: children,
                         ),
                       );
-                    }))
-                // child: FutureBuilder<DeviceList>(
-                //     future: futureDeviceList,
-                //     builder: ((context, snapshot) {
-                //       if (snapshot.hasData) {
-                //         if (snapshot.data!.myDevices != null) {
-                //           final Map<String, String> deviceMap =
-                //               <String, String>{};
-                //           for (var device in snapshot.data!.myDevices!) {
-                //             final deviceNameId = <String, String>{
-                //               device.id: device.displayName
-                //             };
-                //             deviceMap.addEntries(deviceNameId.entries);
-                //           }
-                //           ListView.builder(
-                //               itemCount: deviceMap.length,
-                //               prototypeItem: ListTile(
-                //                 title: Text(deviceMap.values.first),
-                //               ),
-                //               itemBuilder: (context, index) {
-                //                 return ListTile(
-                //                   title: Text(deviceMap.values.elementAt(index)),
-                //                   onTap: () {
-                //                     Navigator.push(
-                //                         context,
-                //                         MaterialPageRoute(
-                //                           builder: (context) => DetailScreen(
-                //                               request: request,
-                //                               deviceId: deviceMap.keys
-                //                                   .elementAt(index)),
-                //                         ));
-                //                   },
-                //                 );
-                //               });
-                //         } else if (snapshot.hasData &&
-                //             snapshot.data!.myDevices == null) {
-                //           return const Text(
-                //               'You have no Yonomi devices linked to your account.');
-                //         }
-                //       } else if (snapshot.hasError) {
-                //         return Text('${snapshot.error}');
-                //       }
-                //       return const CircularProgressIndicator();
-                //     })),
-                )));
+                    })))));
   }
 }
