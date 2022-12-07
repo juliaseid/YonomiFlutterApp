3cc63d5bc5b2a09691db5d406c8d2e22ef980a4d
Julia Seidman <juliaseid@gmail.com>
2022-12-06 23:51:33 -0800

    strips down main.dart to use class, no FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yonomi_device_widgets/traits/detail_screen.dart';
-import 'package:yonomi_flutter_app/request.dart';
-// import 'package:yonomi_flutter_app/devicelist.dart';
-// ignore: library_prefixes
-import 'package:yonomi_platform_sdk/yonomi-sdk.dart' as platform_SDK;
+import 'package:yonomi_platform_sdk/yonomi-sdk.dart' as SDK;
+import 'package:yonomi_flutter_app/devicelist.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,13 +13,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<List<platform_SDK.Device>> futureDeviceList;
-
-  @override
-  void initState() {
-    super.initState();
-    futureDeviceList = platform_SDK.DevicesRepository.getDevices(request);
-  }
+  List<SDK.Device> devices = DeviceList().myDevices;
 
   @override
   Widget build(BuildContext context) {
@@ -31,76 +23,6 @@ class _MyAppState extends State<MyApp> {
             appBar: AppBar(
               title: const Text('Welcome to Yonomi'),
             ),
-            body: Center(
-                child: FutureBuilder<List<platform_SDK.Device>>(
-                    future: futureDeviceList,
-                    builder: ((context,
-                        AsyncSnapshot<List<platform_SDK.Device>> snapshot) {
-                      List<Widget> children;
-                      if (snapshot.hasData) {
-                        List<platform_SDK.Device> devices = snapshot.data!;
-                        children = <Widget>[
-                          const Text("Your Yonomi Devices"),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 16),
-                            child: Text(devices.toString()),
-                            // ListView.builder(
-                            //     itemCount: devices.length,
-                            //     prototypeItem: ListTile(
-                            //       title: Text(devices.first.displayName),
-                            //     ),
-                            //     itemBuilder: (context, index) {
-                            //       return ListTile(
-                            //         title: Text(
-                            //             devices.elementAt(index).displayName),
-                            //         onTap: () {
-                            //           Navigator.push(
-                            //               context,
-                            //               MaterialPageRoute(
-                            //                 builder: (context) =>
-                            //                     DetailScreen(
-                            //                         request: request,
-                            //                         deviceId: devices
-                            //                             .elementAt(index)
-                            //                             .id),
-                            //               ));
-                            //         },
-                            //       );
-                            //     })
-                          )
-                        ];
-                      } else if (snapshot.hasError) {
-                        children = <Widget>[
-                          const Icon(
-                            Icons.error_outline,
-                            color: Colors.red,
-                            size: 60,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 16),
-                            child: Text('Error: ${snapshot.error}'),
-                          ),
-                        ];
-                      } else {
-                        children = const <Widget>[
-                          SizedBox(
-                            width: 60,
-                            height: 60,
-                            child: CircularProgressIndicator(),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 16),
-                            child: Text('Awaiting result...'),
-                          ),
-                        ];
-                      }
-
-                      return Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: children,
-                        ),
-                      );
-                    })))));
+            body: Center(child: Text(devices.toString()))));
   }
 }
